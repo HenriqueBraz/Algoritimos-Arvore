cf1fd3e9e7b9cd8537d77c1050cfc5f6b1d94e7a
HenriqueBraz <38267870+HenriqueBraz@users.noreply.github.com>
2018-06-18 22:19:40 -0300

    Add files via upload

---
+++ src/arvore/Node.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package arvore;
+
+/**
+ *
+ * @author Henrique
+ */
+public class Node<E> {
+    
+    private E valor;
+    private Node Left;
+    private Node Right;
+    private Node Father;
+    
+    public Node(E valor){
+    
+      this.valor = valor;
+      
+    }
+
+    public void setLeft(Node LEFT) {
+        this.Left = LEFT;
+    }
+
+    public void setRight(Node RIGHT) {
+        this.Right = RIGHT;
+    }
+
+    public void setValor(E valor) {
+        this.valor = valor;
+    }
+
+    public void setFather(Node Father) {
+        this.Father = Father;
+    }
+    
+   
+    
+    
+}
+    
+    
+
